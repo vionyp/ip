193de047ebfa78e2d83e246afc989a74affa1690
vionyp <VIONY001@e.ntu.edu.sg>
2026-02-04 10:41:37 +0800

    Level-0: Rename to NIVI and add greet/exit

---
+++ src/main/java/Nivi.java
@@ -0,0 +1,20 @@
+public class Nivi {
+    public static void main(String[] args) {
+        String logo = " _   _  _____ __      __ _____\n"
+                + "| \\ | ||_   _|\\ \\    / /|_   _|\n"
+                + "|  \\| |  | |   \\ \\  / /   | |  \n"
+                + "| . ` |  | |    \\ \\/ /    | |  \n"
+                + "| |\\  | _| |_    \\  /    _| |_ \n"
+                + "|_| \\_||_____|    \\/    |_____|\n";
+
+        System.out.println("Hello from\n" + logo);
+
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm NIVI");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
